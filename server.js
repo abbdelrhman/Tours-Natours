cf97dae1c7648b4d141cc4e501e13e555b02872b
Abdelrahman Hendawy <abdelrahman.hendawy@crealogix.com>
2026-07-05 07:37:08 +0300

    prepare for deployment

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  // console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  // console.log(err.name, err.message);
   process.exit(1);
 });
 
@@ -21,16 +21,16 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => {}); // console.log('DB connection successful!')
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  // console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  // console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  // console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
